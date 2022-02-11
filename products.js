adc67420960b2cb87a466bbc83692eee3978eaf7
DESKTOP-IH915OU\LOMO <fres1102@gmail.com>
2022-02-11 21:43:52 +0800

    20220211

--- products.js
+++ products.js
@@ -1,56 +1,48 @@
 // 使用import  導入vue的 將CDN(沒導入vue就不會出現)
 
 import { createApp } from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js";
+const apiUrl="https://vue3-course-api.hexschool.io/v2";
+const  apiPath= "lomo1986";
 
- createApp({
+createApp({
   data() {
     return {
-      apiUrl: "https://vue3-course-api.hexschool.io/v2",
-      apiPath: "lomo1986",
       products: [],
-      tempProduct: {},
+      tempProduct: {}
     }
   },
+  mounted() {
+    //取出token
+    const Token = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+    axios.defaults.headers.common['Authorization'] = Token;
+    this.checkAdmin();
+     },
   methods: {
     // 確認登入
     checkAdmin() {
-      const Token = document.cookie.replace(/(?:(?:^|.*;\s*)hextoken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
-      axios.defaults.headers.common['Authorization'] = Token;
-
-      const Url = `${this.apiUrl}/api/user/check`;
-      axios.post(Url)
+      axios.post(`${this.apiUrl}/api/user/check`)
       .then((res) => {
         this.getData();
       })
       .catch((err) => {
-        alert(err.data.message)
+        alert(error.data.message);
         //導入產品頁面
-        window.location = `pro.html`;
+        window.location ="pro.html";
       })
     },
     //取得相關資料
     getData() {
-      const api_Url = `${this.apiUrl}/api/${this.apiPath}/admin/products`
-      axios.get(api_Url)
+      axios.get(`${this.apiUrl}/api/${this.apiPath}/admin/products`)
         //接收
-        .then((res => {
+        .then((res) => {
           this.products = res.data.products;
         })
-        .catch((err => {
-         alert(err.data.message);
-       })
+        .catch((err) => {
+            console.log(err);
+        })
+      },
+      tempProduct(item){
+        this.tempProduct = item;
       }
     },
-  //   openProduct(item) {
-  //     this.tempProduct = item;
-  //   }
-  // },
-mounted() {
-  //取出token
- 
-
-  this.checkAdmin()
-   }
-})
-
-app.mount('#app');
+}).mount('#app');
